d0453e171afa8362af1aec2bdf5e5193533436bd
dengyi <yi.deng@minthgroup.com>
2023-08-08 15:06:59 +0800

    修改response对swagger的影响

--- src/main/java/pro/dengyi/myhome/common/config/WebMvcConfig.java
+++ src/main/java/pro/dengyi/myhome/common/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package pro.dengyi.myhome.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,13 +14,15 @@ import pro.dengyi.myhome.common.interceptors.FrameworkInterceptor;
  *
  * @author BLab
  */
-@Configuration
+//@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     private FrameworkInterceptor frameworkInterceptor;
     @Value("${spring.profiles.active}")
-    private String profile;
+    private String profileActive;
+
+    private static final String PROFILE_DEV = "dev";
 
 
     /**
@@ -41,8 +42,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry
-                .addInterceptor(frameworkInterceptor)
-                .addPathPatterns("/**");
+        if (PROFILE_DEV.equals(profileActive)) {
+            registry
+                    .addInterceptor(frameworkInterceptor)
+                    .addPathPatterns("/**")
+                    .excludePathPatterns("/swagger-resources/**")
+                    .excludePathPatterns("/v2/api-docs");
+            ;
+        } else {
+            registry
+                    .addInterceptor(frameworkInterceptor)
+                    .addPathPatterns("/**");
+        }
     }
 }
